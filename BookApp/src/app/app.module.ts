b13df3b77c25266f30685d427d2058cc0a348ac9
scottvang07 <scottvang07@gmail.com>
2017-05-08 22:17:07 -0700

    working main-nav

--- BookApp/src/app/app.module.ts
+++ BookApp/src/app/app.module.ts
@@ -9,14 +9,37 @@ import { BooksComponent } from './books/books.component';
 
 // service
 import { BookService } from './books/book.service';
+import { CategoryService } from './book-category/category.service';
 import { BookCreateComponent } from './books/book-create/book-create.component';
 import { BookComponent } from './books/book/book.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
+import { BookCategoryComponent } from './book-category/book-category.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RecentlyOrderComponent } from './recently-order/recently-order.component';
+
 
 // Routes
 const appRoutes: Routes = [
   { path: '', component: BooksComponent },
-  { path: 'book/:id',   component: BookComponent },
-  { path: 'create',   component: BookCreateComponent }
+  { path: 'book/:id', component: BookComponent },
+  { path: 'cart', component: BookCreateComponent },
+  { path: 'aboutus', component: AboutUsComponent},
+  { path: 'contactus', component: ContactUsComponent},
+  { path: 'orderdetail', component: OrderDetailComponent},
+  { path: 'bookcategory', component: BookCategoryComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: 'recentlyorder', component: RecentlyOrderComponent},
+
+
+
+
+
 ];
 
 @NgModule({
@@ -25,6 +48,15 @@ const appRoutes: Routes = [
     BooksComponent,
     BookCreateComponent,
     BookComponent,
+    MainNavComponent,
+    MainFooterComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    OrderDetailComponent,
+    BookCategoryComponent,
+    LoginComponent,
+    ProfileComponent,
+    RecentlyOrderComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,7 +65,8 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes)
   ],
   providers: [
-    BookService
+    BookService,
+    CategoryService
   ],
   bootstrap: [AppComponent]
 })
